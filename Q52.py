d6db8b0cf14b8554465644651eddbf732fc56c5c
Britney Salazar <98779105+BritneySalazar@users.noreply.github.com>
2022-11-21 23:56:36 -0500

    Add files via upload

--- Q52.py
+++ Q52.py
@@ -1,10 +1,13 @@
 
 quiz = open("text1.txt")
 
-def double_print(count, line):
-    for line in quiz:
-        while count > 0:
-            print(line)
-            count -= 1
+#def double_print(count, line):
+#    for line in quiz:
+        #while count > 0:
+            #rint(line)
+        #    count -= 1
 
-double_print(2, "quiz")
+#double_print(2, "quiz")
+
+for line in quiz:
+    print(line *2)
